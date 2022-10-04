63dc4ae7b4b45ea60140f65234a77818ab455f60
ArthurAardvark <76715144+ArthurAardvark@users.noreply.github.com>
2022-10-04 18:38:27 -0400

    Template Injection

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import { AppStateProvider } from './store' 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import './teleporthq/style.module.css'
-import Image02 from './teleporthq/pages/image02'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const App = () => {
-  return (
-    <Router>
-      <AppStateProvider>
-        <Route exact component={Image02} path="/" />
-      </AppStateProvider>
-    </Router>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('app'))
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
